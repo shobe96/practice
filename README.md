36afa63ee782158c0b4dabc40bba17eceb2766a5
shobe996 <sogorstefan96@gmail.com>
2024-02-25 16:25:02 +0100

    updated pom, properties, sql, and readme file

---
+++ README.md
@@ -0,0 +1,4 @@
+RUN npm install to install all node modules
+
+SWAGGER DOCUMENTATION:
+http://localhost:8080/swagger-ui/index.html
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,13 +1,24 @@
---create schema
 CREATE SCHEMA `employee`;
 
---create user and grant privileges
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
 
 GRANT ALL PRIVILEGES on employee.* TO 'admin'@'localhost';
 
---create employee table
-DROP TABLE IF EXISTS employee
+USE employee;
+
+DROP TABLE IF EXISTS department;
+CREATE TABLE IF NOT EXISTS department (
+    department_id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(25),
+    add_user VARCHAR(25),
+    mod_user VARCHAR(25),
+    add_date TIMESTAMP,
+    mod_date TIMESTAMP,
+    active BOOLEAN,
+    PRIMARY KEY (department_id)
+);
+
+DROP TABLE IF EXISTS employee;
 CREATE TABLE IF NOT EXISTS employee (
     employee_id BIGINT NOT NULL AUTO_INCREMENT,
     name VARCHAR(25),
@@ -21,16 +32,4 @@ CREATE TABLE IF NOT EXISTS employee (
     PRIMARY KEY (employee_id),
     FOREIGN KEY (department_id)
         REFERENCES department (department_id)
-);
-
-DROP TABLE IF EXISTS department
-CREATE TABLE IF NOT EXISTS department (
-    department_id BIGINT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(25),
-    add_user VARCHAR(25),
-    mod_user VARCHAR(25),
-    add_date TIMESTAMP,
-    mod_date TIMESTAMP,
-    active BOOLEAN,
-    PRIMARY KEY (department_id)
 );
