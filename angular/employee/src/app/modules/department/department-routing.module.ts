48eb3299c145cb3a25657f7e6d17a8c7f3ab5e9d
shobe996 <sogorstefan96@gmail.com>
2024-04-06 10:29:45 +0200

    feat: department crud added 2

--- angular/employee/src/app/modules/department/department-routing.module.ts
+++ angular/employee/src/app/modules/department/department-routing.module.ts
@@ -3,16 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { DepartmentEditComponent } from '../../components/department/department-edit/department-edit.component';
 import { DepartmentDetailsComponent } from '../../components/department/department-details/department-details.component';
 import { DepartmentListComponent } from '../../components/department/department-list/department-list.component';
+import { DepartmentComponent } from '../../components/department/department.component';
+import { departmentResolver } from '../../resolvers/department/department.resolver';
 
 const routes: Routes = [
   {
-    path: "", component: DepartmentListComponent, children: [
+    path: "", component: DepartmentComponent, children: [
+      { path: "list", component: DepartmentListComponent },
       { path: "new", component: DepartmentEditComponent },
       { path: "edit/:departmentId", component: DepartmentEditComponent },
-      { path: "details/:departmentId", component: DepartmentDetailsComponent },
+      { path: "details/:departmentId", component: DepartmentDetailsComponent, resolve: { department: departmentResolver } },
     ],
   },
-  { path: "**", component: DepartmentListComponent }
+  { path: "**", component: DepartmentComponent }
 ];
 
 @NgModule({
