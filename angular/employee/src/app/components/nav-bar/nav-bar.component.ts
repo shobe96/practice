c2608a5ea89280626de07908ed301239665e59de
ssogorovic <stefan.sogorovic@eng.it>
2025-01-23 16:05:19 +0100

    added change detection on push

--- angular/employee/src/app/components/nav-bar/nav-bar.component.ts
+++ angular/employee/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,11 +1,12 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { AuthFacadeService } from '../../services/auth/auth.facade.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss',
-  standalone: false
+  standalone: false,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NavBarComponent implements OnInit {
 
