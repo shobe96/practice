64bd80a413c3ea4bbbef267273776453e0b2d953
ssogorovic <stefan.sogorovic@eng.it>
2025-01-29 17:22:53 +0100

    added project edit facade

--- angular/employee/src/app/components/project/project-edit/project-edit.component.ts
+++ angular/employee/src/app/components/project/project-edit/project-edit.component.ts
@@ -1,20 +1,15 @@
 import { Component, EventEmitter, inject, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { ActivatedRoute, Params } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { takeUntil } from 'rxjs';
 import { Project } from '../../../models/project.model';
-import { ProjectService } from '../../../services/project/project.service';
 import { fireToast } from '../../../shared/utils';
 import { Skill } from '../../../models/skill.model';
-import { SkillService } from '../../../services/skill/skill.service';
-import { SkillSearchResult } from '../../../models/skill-search-result.model';
 import { Employee } from '../../../models/employee.model';
 import { EmployeeService } from '../../../services/employee/employee.service';
 import { Department } from '../../../models/department.model';
-import { DepartmentService } from '../../../services/department/department.service';
-import { DepartmentSearchResult } from '../../../models/department-search-result.model';
 import { SubscriptionCleaner } from '../../../shared/subscription-cleaner ';
+import { ProjectEditFacadeService } from '../../../services/project/project-edit.facade.service';
 
 @Component({
   selector: 'app-project-edit',
@@ -23,47 +18,17 @@ import { SubscriptionCleaner } from '../../../shared/subscription-cleaner ';
   standalone: false
 })
 export class ProjectEditComponent extends SubscriptionCleaner implements OnInit, OnDestroy, OnChanges {
-  @Input() public id: number | null = null;
-  @Input() public disable: boolean = false;
-  @Output() public cancelEmiitter: EventEmitter<any> = new EventEmitter();
-  public project: Project = {};
-  public projectFormGroup!: FormGroup;
-  public skills: Skill[] = [];
-  public employees: Employee[] = [];
-  public departments: Department[] = [];
+  @Input() id: number | null = null;
+  @Input() project: Project = {};
+  @Output() cancelEmiitter: EventEmitter<any> = new EventEmitter();
 
-  private route: ActivatedRoute = inject(ActivatedRoute);
-  private projectService: ProjectService = inject(ProjectService);
-  private formBuilder: FormBuilder = inject(FormBuilder);
-  private messageService: MessageService = inject(MessageService);
-  private skillService: SkillService = inject(SkillService);
-  private employeeService: EmployeeService = inject(EmployeeService);
-  private departmentService: DepartmentService = inject(DepartmentService);
+  projectEditFacade: ProjectEditFacadeService = inject(ProjectEditFacadeService);
+  projectFormGroup!: FormGroup;
+
+  private _formBuilder: FormBuilder = inject(FormBuilder);
 
   ngOnInit(): void {
-    this.departmentService.getAllDepartments(true).pipe(takeUntil(this.componentIsDestroyed$)).subscribe({
-      next: (value: DepartmentSearchResult) => {
-        this.departments = value.departments ?? [];
-      },
-      error: (err: any) => {
-        !err.status ? fireToast('error', `${err.statusText}`, `Something went wrong. Conatact admin.`, this.messageService) : fireToast('error', 'Error', `${err.message}`, this.messageService);
-      },
-      complete: () => { }
-    })
-    this.skillService.getAllSkills(true).pipe(takeUntil(this.componentIsDestroyed$)).subscribe({
-      next: (value: SkillSearchResult) => {
-        this.skills = value.skills ?? [];
-      },
-      error: (err: any) => {
-        !err.status ? fireToast('error', `${err.statusText}`, `Something went wrong. Conatact admin.`, this.messageService) : fireToast('error', 'Error', `${err.message}`, this.messageService);
-      },
-      complete: () => { }
-    });
     this.buildForm();
-    this.route.params.pipe(takeUntil(this.componentIsDestroyed$)).subscribe((params: Params) => {
-      this.id = params["projectId"] ?? null;
-      this.initFormFields();
-    });
   }
 
   ngOnDestroy(): void {
@@ -71,97 +36,82 @@ export class ProjectEditComponent extends SubscriptionCleaner implements OnInit,
   }
 
   ngOnChanges(_changes: SimpleChanges): void {
-    this.initFormFields();
+    this._initFormFields();
+    this.projectEditFacade.loadSelectOptions();
   }
 
-  public buildForm() {
-    this.projectFormGroup = this.formBuilder.group({
+  constructor() {
+    super();
+  }
+
+  buildForm() {
+    this.projectFormGroup = this._formBuilder.group({
       name: ['', [Validators.required, Validators.maxLength(25), Validators.minLength(5)]],
       code: ['', [Validators.required, Validators.maxLength(5), Validators.minLength(3)]],
       startDate: [{}, [Validators.required]],
       endDate: [{}, [Validators.required]],
       skills: [[], [Validators.required]],
-      selectedEmployees: [[], [Validators.required]],
-      selectedDepartment: [{}, [Validators.required]]
+      employees: [[], [Validators.required]],
+      department: [{}, [Validators.required]]
     });
   }
 
-  private initFormFields() {
-    if (this.id) {
-      const projectObserver: any = {
-        next: (value: Project) => {
-          this.project = value;
-          this.setValuesToFields(value);
-        },
-        error: (err: any) => { fireToast('error', 'Error', err.error.message, this.messageService); },
-        complete: () => { }
-      };
-      this.projectService.getProject(this.id).pipe(takeUntil(this.componentIsDestroyed$)).subscribe(projectObserver);
-    } else {
-      this.setValuesToFields({});
-    }
+  private _initFormFields() {
+    this._setValuesToFields();
   }
 
-  public cancel(save: boolean) {
+  cancel(save: boolean) {
     this.cancelEmiitter.emit({ visible: false, save: save });
   }
 
-  public submit() {
-    this.project.name = this.projectFormGroup.controls['name'].value;
-    this.project.code = this.projectFormGroup.controls['code'].value;
-    this.project.skills = this.projectFormGroup.controls['skills'].value;
-    this.project.employees = this.projectFormGroup.controls['selectedEmployees'].value;
-    this.project.department = this.projectFormGroup.controls['selectedDepartment'].value;
-    this.project.startDate = this.projectFormGroup.controls['startDate'].value;
-    this.project.endDate = this.projectFormGroup.controls['endDate'].value;
-    const projectObserver: any = {
-      next: (value: Project) => {
-        !this.id ? fireToast("success", "Success", `Project ${value.name} has been created`, this.messageService) : fireToast("success", "Success", `Project ${value.name} has been updated`, this.messageService);
-        this.cancel(true);
-      },
-      error: (err: any) => { fireToast('error', 'Error', err.error.message, this.messageService); },
-      complete: () => { },
-    }
-    !this.id ? this.projectService.save(this.project).pipe(takeUntil(this.componentIsDestroyed$)).subscribe(projectObserver) : this.projectService.update(this.project).pipe(takeUntil(this.componentIsDestroyed$)).subscribe(projectObserver);
-  }
-
-  private retrieveEmployees(skills: Skill[], department: Department) {
-    if (skills.length > 0 && Object.keys(department).length > 0) {
-      this.employeeService.filterEmployeesByActiveAndSkills(skills, department).pipe(takeUntil(this.componentIsDestroyed$)).subscribe({
-        next: (value: Employee[]) => {
-          this.employees = value;
-        },
-        error: (err: any) => { fireToast('error', 'Error', err.error.message, this.messageService); },
-        complete: () => { }
+  submit() {
+    this.project = this._getFormValues();
+    this.projectEditFacade.submit(this.project)
+      .pipe(takeUntil(this.componentIsDestroyed$))
+      .subscribe((value: Project) => {
+        if (Object.keys(value)) {
+          this.cancel(true);
+        }
       });
-    }
   }
 
-  public onChanges(_event: any) {
-    const department = this.project.department = this.projectFormGroup.controls['selectedDepartment'].value;
+  getEmployees(_event: any) {
+    const department = this.project.department = this.projectFormGroup.controls['department'].value;
     const skills = this.projectFormGroup.controls['skills'].value;
-    this.retrieveEmployees(skills, department);
+    this.projectFormGroup.controls['employees'].setValue([]);
+    this.projectEditFacade.getEmployees(skills, department);
   }
 
-  private setValuesToFields(value: Project) {
-    if (this.projectFormGroup) {
-      const name = value.name ?? '';
-      const code = value.code ?? ''
-      const startDate = value.startDate ?? new Date();
-      const endDate = value.endDate ?? new Date();
-      const employees = value.employees ?? [];
-      const department = value.department ?? {};
-      const skills = value.skills ?? [];
-      this.retrieveEmployees(skills, department);
+  private _setValuesToFields() {
+
+    if (this.project) {
+      const name = this.project.name ?? '';
+      const code = this.project.code ?? ''
+      const employees = this.project.employees ?? [];
+      const department = this.project.department ?? {};
+      const skills = this.project.skills ?? [];
+      const startDate = this.project.startDate ? new Date(this.project.startDate) : new Date();
+      const endDate = this.project.endDate ? new Date(this.project.endDate) : new Date();
+
       if (this.projectFormGroup) {
         this.projectFormGroup.controls['name'].setValue(name);
         this.projectFormGroup.controls['code'].setValue(code);
+        this.projectFormGroup.controls['department'].setValue(department);
         this.projectFormGroup.controls['skills'].setValue(skills);
-        this.projectFormGroup.controls['selectedDepartment'].setValue(department);
-        this.projectFormGroup.controls['selectedEmployees'].setValue(employees);
-        this.projectFormGroup.controls['startDate'].setValue(new Date(startDate));
-        this.projectFormGroup.controls['endDate'].setValue(new Date(endDate));
+        this.getEmployees(null);
+        this.projectFormGroup.controls['startDate'].setValue(startDate);
+        this.projectFormGroup.controls['endDate'].setValue(endDate);
+        this.projectFormGroup.controls['employees'].setValue(employees);
       }
+      if (Object.keys(this.project)?.length === 1) this.projectEditFacade.clearEmployees();
     }
+  }
+
+  private _getFormValues(): Project {
+    const project: Project = { ...this.project };
+    for (const field in this.projectFormGroup.controls) {
+      project[field] = this.projectFormGroup.controls[field].value;
+    }
+    return project;
   }
 }
--- angular/employee/src/app/models/project.model.ts
+++ angular/employee/src/app/models/project.model.ts
@@ -3,6 +3,7 @@ import { Employee } from "./employee.model";
 import { Skill } from "./skill.model";
 
 export interface Project {
+  [key: string]: number | string | undefined | Department | Date | Skill[] | Employee[];
   id?: number;
   name?: string;
   code?: string;
