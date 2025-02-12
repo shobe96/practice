747c93219cbf1d4ffa0407684ccc5c2332c8a66f
ssogorovic <stefan.sogorovic@eng.it>
2025-02-12 14:45:49 +0100

    removed table hover directive

--- angular/employee/src/app/modules/shared/shared.module.ts
+++ angular/employee/src/app/modules/shared/shared.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TableHoverDirective } from '../../shared/directives/table-hover.directive';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MessageService } from 'primeng/api';
 
@@ -10,14 +9,10 @@ const MODULES = [
 ];
 
 @NgModule({
-  declarations: [
-    TableHoverDirective,
-  ],
   imports: [
     MODULES
   ],
   exports: [
-    TableHoverDirective,
     MODULES
   ],
   providers: [MessageService]
--- angular/employee/src/app/shared/directives/table-hover.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TableHoverDirective } from './table-hover.directive';
-
-describe('TableHoverDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TableHoverDirective();
-    expect(directive).toBeTruthy();
-  });
-});
