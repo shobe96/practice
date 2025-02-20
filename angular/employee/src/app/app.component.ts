4cf6d9dc1d3cae72aca46885da08d7fa091d5ed1
ssogorovic <stefan.sogorovic@eng.it>
2025-02-20 11:12:07 +0100

    app component converted to standalone

--- angular/employee/src/app/app.component.ts
+++ angular/employee/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-  standalone: false
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [NavBarComponent, RouterOutlet]
 })
 export class AppComponent {
   title = 'employee';
--- angular/employee/src/main.ts
+++ angular/employee/src/main.ts
@@ -1,7 +1,31 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import { authInterceptor } from './app/shared/interceptors/auth/auth.interceptor';
+import { providePrimeNG } from 'primeng/config';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app/app-routing.module';
+import { SharedModule } from './app/modules/shared/shared.module';
+import { ToastModule } from 'primeng/toast';
+import { MenubarModule } from 'primeng/menubar';
+import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import Aura from '@primeng/themes/aura';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserModule, AppRoutingModule, SharedModule, ToastModule, MenubarModule),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withInterceptorsFromDi()),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    }),
+    provideAnimations()
+  ]
+})
   .catch(err => console.error(err));
