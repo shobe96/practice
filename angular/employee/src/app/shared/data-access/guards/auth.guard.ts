79c49f821a233e1407c49610191f67c46d481b8a
ssogorovic <stefan.sogorovic@eng.it>
2025-07-07 17:45:39 +0200

    removed unnecessary typing

--- angular/employee/src/app/shared/data-access/guards/auth.guard.ts
+++ angular/employee/src/app/shared/data-access/guards/auth.guard.ts
@@ -1,13 +1,12 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthResponse } from '../../../auth/data-access/auth-response.model';
-import { Role } from '../../../roles/data-access/role.model';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authResponse = localStorage.getItem('authResponse');
   if (authResponse) {
     const json: AuthResponse = JSON.parse(authResponse);
-    const userRoles: Role[] = json.roles ?? [];
+    const userRoles = json.roles ?? [];
     const roles: string[] = route.data['roles'] ?? [];
     for (const userRole of userRoles) {
       for (const role of roles) {
