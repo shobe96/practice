43a641ddf22dbcd2593803f9eb53bc6ca1213146
ssogorovic <stefan.sogorovic@eng.it>
2025-07-08 17:52:20 +0200

    removed unused import

--- angular/employee/src/app/employees/data-access/employee-list.facade.service.ts
+++ angular/employee/src/app/employees/data-access/employee-list.facade.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Employee } from './employee.model';
 import { EmployeeService } from './employee.service';
 import { BaseListFacade } from '../../shared/data-access/services/base/base-list.facade';
-import { SearchResult } from '../../shared/data-access/search-result.model';
 
 @Injectable()
 export class EmployeeListFacadeService extends BaseListFacade<Employee> {
