0b9b20d7d93f4343f771fee285e2739f2dfb79db
ssogorovic <stefan.sogorovic@eng.it>
2025-07-08 09:30:55 +0200

    removed unused imports

--- angular/employee/src/app/departments/data-access/department-edit.facade.service.ts
+++ angular/employee/src/app/departments/data-access/department-edit.facade.service.ts
@@ -1,8 +1,6 @@
-import { inject, Injectable } from '@angular/core';
-import { BehaviorSubject, catchError, combineLatest, finalize, map, Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
 import { Department } from './department.model';
 import { DepartmentService } from './department.service';
-import { CustomMessageService } from '../../shared/data-access/services/custom-message/custom-message.service';
 import { BaseEditFacade } from '../../shared/data-access/services/base/base-edit.facade';
 
 @Injectable()
